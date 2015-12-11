a3cfa544fc2a63754355b6e144713c9e746dae9e
fooklook <xiashuo.he@foxmail.com>
2015-12-11 18:00:55 +0800

    qiniu-images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function(){
 	if(window.screen.width < 900){
-		var imgsrc = ['images/bg-001-mobile.jpg', 'images/bg-002-mobile.jpg', 'images/bg-003-mobile.jpg'];
+		var imgsrc = [
+			'http://7xp3vm.com1.z0.glb.clouddn.com/bg-001-mobile.jpg', 
+			'http://7xp3vm.com1.z0.glb.clouddn.com/bg-002-mobile.jpg', 
+			'http://7xp3vm.com1.z0.glb.clouddn.com/bg-003-mobile.jpg'
+		];
 		$("#myCarousel img").each(function(index){
 			$(this).attr("src",imgsrc[index]);
 		});
